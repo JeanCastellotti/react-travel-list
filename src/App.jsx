14a03ee9a14ef22f9d0576cbc1dc89493238aa26
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-06-08 17:20:09 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Logo from './components/Logo'
+import Form from './components/Form'
+import PackingList from './components/PackingList'
+import Stats from './components/Stats'
+
+export default function App() {
+  return <h1 className="text-2xl font-medium">Travel List</h1>
+}
